d4d392f8e6b4ba66728f0419233d5a006eaaada7
David Harris <dharris@dharris.io>
2019-07-09 17:34:09 -0600

    Update to Terraform 0.12

--- iam.tf
+++ iam.tf
@@ -13,14 +13,14 @@ data "aws_iam_policy_document" "assume_role" {
 }
 
 resource "aws_iam_role" "lambda" {
-  name               = "${var.function_name}"
-  assume_role_policy = "${data.aws_iam_policy_document.assume_role.json}"
+  name               = var.function_name
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
 # Attach a policy for logs.
 
 data "aws_iam_policy_document" "logs" {
-  count = "${var.enable_cloudwatch_logs ? 1 : 0}"
+  count = var.enable_cloudwatch_logs ? 1 : 0
 
   statement {
     effect = "Allow"
@@ -49,31 +49,31 @@ data "aws_iam_policy_document" "logs" {
 }
 
 resource "aws_iam_policy" "logs" {
-  count = "${var.enable_cloudwatch_logs ? 1 : 0}"
+  count = var.enable_cloudwatch_logs ? 1 : 0
 
   name   = "${var.function_name}-logs"
-  policy = "${data.aws_iam_policy_document.logs.json}"
+  policy = data.aws_iam_policy_document.logs[0].json
 }
 
 resource "aws_iam_policy_attachment" "logs" {
-  count = "${var.enable_cloudwatch_logs ? 1 : 0}"
+  count = var.enable_cloudwatch_logs ? 1 : 0
 
   name       = "${var.function_name}-logs"
-  roles      = ["${aws_iam_role.lambda.name}"]
-  policy_arn = "${aws_iam_policy.logs.arn}"
+  roles      = [aws_iam_role.lambda.name]
+  policy_arn = aws_iam_policy.logs[0].arn
 }
 
 resource "aws_cloudwatch_log_group" "logs" {
-  count = "${var.enable_cloudwatch_logs ? 1 : 0}"
+  count = var.enable_cloudwatch_logs ? 1 : 0
 
   name = "/aws/lambda/${var.function_name}"
-  tags = "${var.tags}"
+  tags = var.tags
 }
 
 # Attach an additional policy required for the dead letter config.
 
 data "aws_iam_policy_document" "dead_letter" {
-  count = "${var.attach_dead_letter_config ? 1 : 0}"
+  count = var.attach_dead_letter_config ? 1 : 0
 
   statement {
     effect = "Allow"
@@ -83,25 +83,33 @@ data "aws_iam_policy_document" "dead_letter" {
       "sqs:SendMessage",
     ]
 
+    # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
+    # force an interpolation expression to be interpreted as a list by wrapping it
+    # in an extra set of list brackets. That form was supported for compatibilty in
+    # v0.11, but is no longer supported in Terraform v0.12.
+    #
+    # If the expression in the following list itself returns a list, remove the
+    # brackets to avoid interpretation as a list of lists. If the expression
+    # returns a single list item then leave it as-is and remove this TODO comment.
     resources = [
-      "${lookup(var.dead_letter_config, "target_arn", "")}",
+      lookup(var.dead_letter_config, "target_arn", ""),
     ]
   }
 }
 
 resource "aws_iam_policy" "dead_letter" {
-  count = "${var.attach_dead_letter_config ? 1 : 0}"
+  count = var.attach_dead_letter_config ? 1 : 0
 
   name   = "${var.function_name}-dl"
-  policy = "${data.aws_iam_policy_document.dead_letter.json}"
+  policy = data.aws_iam_policy_document.dead_letter[0].json
 }
 
 resource "aws_iam_policy_attachment" "dead_letter" {
-  count = "${var.attach_dead_letter_config ? 1 : 0}"
+  count = var.attach_dead_letter_config ? 1 : 0
 
   name       = "${var.function_name}-dl"
-  roles      = ["${aws_iam_role.lambda.name}"]
-  policy_arn = "${aws_iam_policy.dead_letter.arn}"
+  roles      = [aws_iam_role.lambda.name]
+  policy_arn = aws_iam_policy.dead_letter[0].arn
 }
 
 # Attach an additional policy required for the VPC config
@@ -123,33 +131,34 @@ data "aws_iam_policy_document" "network" {
 }
 
 resource "aws_iam_policy" "network" {
-  count = "${var.attach_vpc_config ? 1 : 0}"
+  count = var.attach_vpc_config ? 1 : 0
 
   name   = "${var.function_name}-network"
-  policy = "${data.aws_iam_policy_document.network.json}"
+  policy = data.aws_iam_policy_document.network.json
 }
 
 resource "aws_iam_policy_attachment" "network" {
-  count = "${var.attach_vpc_config ? 1 : 0}"
+  count = var.attach_vpc_config ? 1 : 0
 
   name       = "${var.function_name}-network"
-  roles      = ["${aws_iam_role.lambda.name}"]
-  policy_arn = "${aws_iam_policy.network.arn}"
+  roles      = [aws_iam_role.lambda.name]
+  policy_arn = aws_iam_policy.network[0].arn
 }
 
 # Attach an additional policy if provided.
 
 resource "aws_iam_policy" "additional" {
-  count = "${var.attach_policy ? 1 : 0}"
+  count = var.attach_policy ? 1 : 0
 
-  name   = "${var.function_name}"
-  policy = "${var.policy}"
+  name   = var.function_name
+  policy = var.policy
 }
 
 resource "aws_iam_policy_attachment" "additional" {
-  count = "${var.attach_policy ? 1 : 0}"
+  count = var.attach_policy ? 1 : 0
 
-  name       = "${var.function_name}"
-  roles      = ["${aws_iam_role.lambda.name}"]
-  policy_arn = "${aws_iam_policy.additional.arn}"
+  name       = var.function_name
+  roles      = [aws_iam_role.lambda.name]
+  policy_arn = aws_iam_policy.additional[0].arn
 }
+
--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,46 @@
 output "function_arn" {
   description = "The ARN of the Lambda function"
-  value       = "${element(concat(aws_lambda_function.lambda.*.arn, aws_lambda_function.lambda_with_dl.*.arn, aws_lambda_function.lambda_with_vpc.*.arn, aws_lambda_function.lambda_with_dl_and_vpc.*.arn), 0)}"
+  value = element(
+    concat(
+      aws_lambda_function.lambda.*.arn,
+      aws_lambda_function.lambda_with_dl.*.arn,
+      aws_lambda_function.lambda_with_vpc.*.arn,
+      aws_lambda_function.lambda_with_dl_and_vpc.*.arn,
+    ),
+    0,
+  )
 }
 
 output "function_name" {
   description = "The name of the Lambda function"
-  value       = "${element(concat(aws_lambda_function.lambda.*.function_name, aws_lambda_function.lambda_with_dl.*.function_name, aws_lambda_function.lambda_with_vpc.*.function_name, aws_lambda_function.lambda_with_dl_and_vpc.*.function_name), 0)}"
+  value = element(
+    concat(
+      aws_lambda_function.lambda.*.function_name,
+      aws_lambda_function.lambda_with_dl.*.function_name,
+      aws_lambda_function.lambda_with_vpc.*.function_name,
+      aws_lambda_function.lambda_with_dl_and_vpc.*.function_name,
+    ),
+    0,
+  )
 }
 
 output "role_arn" {
   description = "The ARN of the IAM role created for the Lambda function"
-  value       = "${aws_iam_role.lambda.arn}"
+  value       = aws_iam_role.lambda.arn
 }
 
 output "role_name" {
   description = "The name of the IAM role created for the Lambda function"
-  value       = "${aws_iam_role.lambda.name}"
+  value       = aws_iam_role.lambda.name
 }
 
 output "log_group_arn" {
   description = "The ARN of the cloudwatch log group (if enabled)"
-  value       = "${element(concat(aws_cloudwatch_log_group.logs.*.arn, list("")), 0)}"
+  value       = element(concat(aws_cloudwatch_log_group.logs.*.arn, [""]), 0)
 }
 
 output "log_group_name" {
   description = "The name of the cloudwatch log group (if enabled)"
-  value       = "${element(concat(aws_cloudwatch_log_group.logs.*.name, list("")), 0)}"
+  value       = element(concat(aws_cloudwatch_log_group.logs.*.name, [""]), 0)
 }
+
